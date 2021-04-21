abbf689331895435af8809231fc30dd3d24f60f4
Debora Durek <debora.durek@gmail.com>
2021-04-20 21:28:32 -0500

    feat: Create state and method to get state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function createStore() {
+  // The store should have four parts:
+  // 1. The state.
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state.
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // 1 State
+  let state;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // 2 Get
+  const getState = () => state;
+
+  return getState;
+}
